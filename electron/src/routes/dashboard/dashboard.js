8fc47b4130a9a06e87bbdd66981c797c7b9b7c72
Madeline Helmstadter <madeline.helmstadter@gmail.com>
2018-09-19 14:46:20 -0400

    Moved project to React.js

--- electron/src/routes/dashboard/dashboard.js
+++
@@ -1,8 +0,0 @@
-const remote = require('electron').remote
-const main = remote.require('./main.js')
-
-function openLoginWindow() {
-    var currWindow = remote.getCurrentWindow();
-    main.openWindow("/src/routes/login/index");
-    currWindow.close();
-}
