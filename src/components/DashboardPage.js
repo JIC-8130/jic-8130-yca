a68f3d87ae07082fca9d78984006045dd6f85254
emanuallan <allanserna@lawn-128-61-77-245.lawn.gatech.edu>
2018-10-19 16:02:10 -0400

    Initial Data visualization added.

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -8,100 +8,91 @@ import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+
 import fusioncharts from 'fusioncharts';
 // Load the charts module
 import charts from 'fusioncharts/fusioncharts.charts';
 import ReactFC from 'react-fusioncharts';
 
-// Pass fusioncharts as a dependency of charts
+import ReactChartkick, { LineChart} from 'react-chartkick'
+import Chart from 'chart.js'
 
-charts(FusionCharts)
+//const CHART = document.getElementById("lineChart");
+//console.log("chart" + CHART); //null
 
-var chartConfigs = {
-  
-    type: "Column2D",
-   width:"80%",height:400,
-    className: "fc-column2d", // ReactJS attribute-name for DOM classes 1
-    dataFormat: "JSON",
-    dataSource: {
-        chart:{ caption:"Harry's SuperMart",subCaption:"Top 5 stores in last month by revenue",numberPrefix:"$", theme: "zune",
-        palettecolors: "#0075c2"},
-        data: [{label:"Bakersfield Central",value:"880000"},{label:"Garden Groove harbour",value:"730000"},{label:"Los Angeles Topanga",value:"590000"},{label:"Compton-Rancho Dom",value:"520000"},{label:"Daly City Serramonte",value:"330000"}]
+var producedData = {
+    label: "Number of Units Produced",
+    data: [120, 80, 75, 94, 103, 67],
+    lineTension: 0.3,
+    fill: false,
+    borderColor: 'rgba(51, 255, 107, 0.4)',
+    backgroundColor: 'rgba(51, 255, 107, 0.5)',
+    pointBorderColor: 'rgba(51, 255, 107, 0.4)',
+    pointBackgroundColor: 'rgba(51, 255, 107, 1)',
+    pointRadius: 5,
+    pointHoverRadius: 15,
+    pointHitRadius: 30,
+    pointBorderWidth: 2,
+    pointStyle: 'circle',
+    // Set More Options
+  };
+     
+  var defectsData = {
+    label: "Number of Defects",
+    data: [10, 5, 11, 2, 6, 4],
+    lineTension: 0.3,
+    lineTension: 0.3,
+    fill: false,
+    borderColor: 'rgba(199, 0, 57, 0.4)',
+    backgroundColor: 'rgba(199, 0, 57, 0.4)',
+    borderColor: 'rgba(199, 0, 57, 0.4)',
+    pointBackgroundColor: 'rgba(199, 0, 57, 1)',
+    pointRadius: 5,
+    pointHoverRadius: 15,
+    pointHitRadius: 30,
+    pointBorderWidth: 2,
+    pointStyle: 'circle'
+  };
+
+  var qiData = {
+    label: "Number of Quality Incidents",
+    data: [15, 10, 17, 7, 2, 11],
+    lineTension: 0.3,
+    lineTension: 0.3,
+    fill: false,
+    borderColor: 'rgba(255, 195, 0, 0.4)',
+    backgroundColor: 'rgba(255, 195, 0, 0.4)',
+    pointBorderColor: 'rgba(255, 195, 0, 0.4)',
+    pointBackgroundColor: 'rgba(255, 195, 0, 1)',
+    pointRadius: 5,
+    pointHoverRadius: 15,
+    pointHitRadius: 30,
+    pointBorderWidth: 2,
+    pointStyle: 'circle'
+  };
+     
+  var productionData = {
+    labels: ["10-15-2018", "10-16-2018", "10-17-2018", "10-18-2018", "10-19-2018", "10-20-2018"],
+    datasets: [producedData, defectsData, qiData]
+  };
+
+  var chartOptions = {
+    legend: {
+      display: true,
+      position: 'top',
+      labels: {
+        boxWidth: 80,
+        fontColor: 'black'
+      }
     }
-};
+  };
 
-var pieChartConfig={
-
-type:"Pie3d",
-width:"80%",
-height:"400",
-className:'fc-pie3d',
-dataFormat:"JSON",
-dataSource:
-  {
-    chart: {
-        caption: "Sales Per Employee for 2014",
-        "palette": "2",
-        "animation": "1",
-        "formatnumberscale": "1",
-        "decimals": "0",
-        "numberprefix": "$",
-        "pieslicedepth": "30",
-        "startingangle": "125",
-        "showborder": "0"
-    },
-    "data": [
-        {
-            "label": "Leverling",
-            "value": "100524",
-            "issliced": "1"
-        },
-        {
-            "label": "Fuller",
-            "value": "87790",
-            "issliced": "1"
-        },
-        {
-            "label": "Davolio",
-            "value": "81898",
-            "issliced": "0"
-        },
-        {
-            "label": "Peacock",
-            "value": "76438",
-            "issliced": "0"
-        },
-        {
-            "label": "King",
-            "value": "57430",
-            "issliced": "0"
-        },
-        {
-            "label": "Callahan",
-            "value": "55091",
-            "issliced": "0"
-        },
-        {
-            "label": "Dodsworth",
-            "value": "43962",
-            "issliced": "0"
-        },
-        {
-            "label": "Suyama",
-            "value": "22474",
-            "issliced": "0"
-        },
-        {
-            "label": "Buchanan",
-            "value": "21637",
-            "issliced": "0"
-        }
-    ]
-}
-
-
-};
-
+var ctx = document.getElementById("lineChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: productionData,
+    options: chartOptions
+});
 
 const HomePage = () => (
     <React.Fragment>
@@ -176,10 +167,10 @@ const HomePage = () => (
             </Grid>
 
             <Grid item xs={5} style={{marginLeft:20}}>
-                <ReactFC {...chartConfigs} />
+
             </Grid>
             <Grid item xs={5}>
-                <ReactFC {...pieChartConfig}/>
+                {/* chart1 */}
             </Grid>
 
             <Grid item xs={12} align="right">
@@ -195,3 +186,5 @@ const HomePage = () => (
 );
 
 export default HomePage;
+
+HomePage();
