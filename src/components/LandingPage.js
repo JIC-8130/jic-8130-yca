5686f7ebf7bf8f8ed7dc519479ada3b6c0b4c9f2
emanuallan <allanserna@lawn-128-61-68-119.lawn.gatech.edu>
2018-11-09 11:08:22 -0500

    repeat of the last push, create button -> login screen

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,52 +7,53 @@ import bulb from '../assets/img/yokogawa-logo.png';
 import {Link} from 'react-router-dom';
 
 const LandingPage = () => (
-  <div className="landingPagebodyComponent">
-      <Typography variant="display3" gutterBottom align="center">
-          Welcome to ASGARD
-      </Typography>
+    <div className="landingPagebodyComponent">
+        <Typography variant="display3" gutterBottom align="center">
+            Welcome to ASGARD
+        </Typography>
 
-      <Grid container spacing={24} justify="center" alignContent="center" alignItems="center">
-          <Grid item xs={12} md={12}>
-              <Typography variant="body2" gutterBottom align="center">
-                  YCA's Cost Center Data Collection Tool
-              </Typography>
-          </Grid>
+        <Grid container spacing={24} justify="center" alignContent="center" alignItems="center">
+            <Grid item xs={12} md={12}>
+                <Typography variant="body2" gutterBottom align="center">
+                    YCA's Cost Center Data Collection Tool
+                </Typography>
+            </Grid>
 
-          <Grid container item xs={6} justify="center" alignContent="center" alignItems="center" direction="column">
-              <Typography gutterBottom align="center" style={{paddingLeft:20}}>
-              {`
-                  The application we have designed is called ASGARD, which stands for Assembly Statistics Gatherer and Recorder. The core functionality of our application is broken down into 2 parts: data gathering and data reporting.            `}
-              </Typography>
+            <Grid container item xs={6} justify="center" alignContent="center" alignItems="center" direction="column">
+                <Typography gutterBottom align="center" style={{paddingLeft:20}}>
+                {`
+                  The application we have designed is called ASGARD, which stands for Assembly Statistics Gatherer and Recorder. The core functionality of our application is broken down into 2 parts: data gathering and data reporting.            
+                `}
+                </Typography>
 
-              <p></p>
+                <p></p>
 
-              <Typography variant="body2" gutterBottom align="center" style={{paddingLeft:20}}>
-                  {`Get started by creating an account.   `}
-              </Typography>
+                <Typography variant="body2" gutterBottom align="center" style={{paddingLeft:20}}>
+                    {`Get started by creating an account.   `}
+                </Typography>
 
-              <Link to="/create-account">
-                  <Button color="primary"  align="left" style={{marginLeft:20}}>
-                      Create Account
-                  </Button>
-              </Link>
+                <Link to="/create-account">
+                    <Button color="primary"  align="left" style={{marginLeft:20}}>
+                        Create Account
+                    </Button>
+                </Link>
 
-              {/* <Link to ="/">
-                  <Button color="primary"  align="left" style={{marginLeft:20}}>
-                      Know More
-                  </Button>
-              </Link> */}
-          </Grid>
-      </Grid>
+                {/* <Link to ="/">
+                    <Button color="primary"  align="left" style={{marginLeft:20}}>
+                        Know More
+                    </Button>
+                </Link> */}
+            </Grid>
+        </Grid>
 
-      <Grid item xs={8}>
-          <Image
-              src={bulb}
-              color="inherit" style={{height:40}} imageStyle={{ width: '30', height: '30' }} />
-      </Grid>
+        <Grid item xs={8}>
+            <Image
+                src={bulb}
+                color="inherit" style={{height:40}} imageStyle={{ width: '30', height: '30' }} />
+        </Grid>
 
 
-  </div>
+    </div>
 );
 
 export default LandingPage;
