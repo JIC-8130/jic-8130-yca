3bec7ffcb278585524c2088eb9545155353e8c12
emanuallan <allanserna@Allans-MacBook-Pro.local>
2018-11-21 13:39:53 -0600

    Populating rest of Graph variables

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -152,13 +152,69 @@ function extractUnits(data) {
     return retVals;
 }
 
+function extractDefects(data) {
+    var retVals = []
+    data.forEach(row => {
+        retVals.push(row.Defects);
+    });
+    return retVals;
+}
+
+function extractWorkerTotal(data) {
+    var retVals = []
+    data.forEach(row => {
+        retVals.push(row.WorkerTotal);
+    });
+    return retVals;
+}
+
+function extractSInc_Num(data) {
+    var retVals = []
+    data.forEach(row => {
+        retVals.push(row.SInc_Num);
+    });
+    return retVals;
+}
+
+function extractQInc_Num(data) {
+    var retVals = []
+    data.forEach(row => {
+        retVals.push(row.QInc_Num);
+    });
+    return retVals;
+}
+
+function extractOvertime(data) {
+    var retVals = []
+    data.forEach(row => {
+        retVals.push(row.Overtime);
+    });
+    return retVals;
+}
+
+function extractDowntime(data) {
+    var retVals = []
+    data.forEach(row => {
+        retVals.push(row.Downtime);
+    });
+    return retVals;
+}
+
 export class DashboardPage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+
             costCenterSelected: "",
             producedData: {},
+            defectsData: {},
+            qiData: {},
+            safetyData: {},
+            workerData: {},
+            overtimeData: {},
+            downtimeData: {},
+
             loadingData: false
         }
         this.onViewTableClick = this.onViewTableClick.bind(this);
@@ -198,21 +254,121 @@ export class DashboardPage extends React.Component {
                 .then(data => this.setState({
                     producedData: {
                         labels: ["10-15-2018", "10-16-2018", "10-17-2018", "10-18-2018"],
-                        datasets: [{
-                            label: "Number of Units Produced",
-                            data: extractUnits(data),
-                            lineTension: 0.3,
-                            fill: false,
-                            borderColor: 'rgba(51, 255, 107, 0.4)',
-                            backgroundColor: 'rgba(51, 255, 107, 0.5)',
-                            pointBorderColor: 'rgba(51, 255, 107, 0.4)',
-                            pointBackgroundColor: 'rgba(51, 255, 107, 1)',
-                            pointRadius: 5,
-                            pointHoverRadius: 15,
-                            pointHitRadius: 30,
-                            pointBorderWidth: 2,
-                            pointStyle: 'circle'
-                        }]
+                        datasets:
+                            [
+                                {
+                                    label: "Number of Units Produced",
+                                    data: extractUnits(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'rgba(51, 255, 107, 0.4)',
+                                    backgroundColor: 'rgba(51, 255, 107, 0.5)',
+                                    pointBorderColor: 'rgba(51, 255, 107, 0.4)',
+                                    pointBackgroundColor: 'rgba(51, 255, 107, 1)',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                },
+
+                                {
+                                    label: "Number of Defects",
+                                    data: extractDefects(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'rgba(199, 0, 57, 0.4)',
+                                    backgroundColor: 'rgba(199, 0, 57, 0.4)',
+                                    borderColor: 'rgba(199, 0, 57, 0.4)',
+                                    pointBackgroundColor: 'rgba(199, 0, 57, 1)',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                },
+
+                                {
+                                    label: "Number of Workers at Line",
+                                    data: extractWorkerTotal(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'rgba(51, 134, 255, 0.4)',
+                                    backgroundColor: 'rgba(51, 134, 255, 0.4)',
+                                    borderColor: 'rgba(51, 134, 255, 0.4)',
+                                    pointBackgroundColor: 'rgba(51, 134, 255, 1)',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                },
+
+                                {
+                                    label: "Number of Safety Incidents",
+                                    data: extractSInc_Num(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'red',
+                                    backgroundColor: 'red',
+                                    borderColor: 'red',
+                                    pointBackgroundColor: 'red',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                },
+
+                                {
+                                    label: "Number of Quality Incidents",
+                                    data: extractQInc_Num(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'rgba(255, 195, 0, 0.4)',
+                                    backgroundColor: 'rgba(255, 195, 0, 0.4)',
+                                    pointBorderColor: 'rgba(255, 195, 0, 0.4)',
+                                    pointBackgroundColor: 'rgba(255, 195, 0, 1)',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                },
+
+                                {
+                                    label: "Assembly Line Overtime",
+                                    data: extractOvertime(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'rgba(95, 83, 100, 0.4)',
+                                    backgroundColor: 'rgba(95, 83, 100, 0.4)',
+                                    borderColor: 'rgba(95, 83, 100, 0.4)',
+                                    pointBackgroundColor: 'rgba(95, 83, 100, 1)',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                },
+
+                                {
+                                    label: "Assembly Line Downtime",
+                                    data: extractDowntime(data),
+                                    lineTension: 0.3,
+                                    fill: false,
+                                    borderColor: 'rgba(255, 159, 51, 0.7)',
+                                    backgroundColor: 'rgba(255, 159, 51, 0.7)',
+                                    borderColor: 'rgba(255, 159, 51, 0.7)',
+                                    pointBackgroundColor: 'rgba(255, 159, 51, 1)',
+                                    pointRadius: 5,
+                                    pointHoverRadius: 15,
+                                    pointHitRadius: 30,
+                                    pointBorderWidth: 2,
+                                    pointStyle: 'circle'
+                                }
+
+                            ]
                         // Set More Options
                     },
                     loadingData: false
@@ -257,11 +413,11 @@ export class DashboardPage extends React.Component {
                     <Grid item xs={3} style={{ marginLeft: 45 }}>
                         <Typography variant="subheading" gutterBottom align="left">
                             <ul>
-                                <li><a href="#"> 6212 YDC Receiving </a></li>
-                                <li><a href="#"> 6213 YDC Stores </a></li>
-                                <li><a href="#"> 6234 YC Shipping </a></li>
-                                <li><a href="#"> 6322 Parts & Repair </a></li>
-                                <li><a href="#"> 6401 Parts & Manufacturing </a></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6212", loadingData: true }) }}> 6212 YDC Receiving </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6213", loadingData: true }) }}> 6213 YDC Stores </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6234", loadingData: true }) }}> 6234 YC Shipping </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6322", loadingData: true }) }}> 6322 Parts & Repair </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6401", loadingData: true }) }}> 6401 Parts & Manufacturing </Button></li>
                             </ul>
                         </Typography>
                     </Grid>
@@ -269,11 +425,11 @@ export class DashboardPage extends React.Component {
                     <Grid item xs={3}>
                         <Typography variant="subheading" gutterBottom align="left">
                             <ul>
-                                <li><a href="#"> 6422 Machine Shop </a></li>
-                                <li><a href="#"> 6511 SMT </a></li>
-                                <li><a href="#"> 6521 Meter Assembly </a></li>
-                                <li><Button variant="outlined" onClick={() => { this.setState({ costCenterSelected: "CC6522", loadingData: true }) }}> 6522 Analytical Manufacturing </Button></li>
-                                <li><Button variant="outlined" onClick={() => { this.setState({ costCenterSelected: "CC6526", loadingData: true }) }}> 6526 Transmitter Manufacturing </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6422", loadingData: true }) }}> 6422 Machine Shop </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6511", loadingData: true }) }}> 6511 SMT </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6521", loadingData: true }) }}> 6521 Meter Assembly </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6522", loadingData: true }) }}> 6522 Analytical Manufacturing </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6526", loadingData: true }) }}> 6526 Transmitter Manufacturing </Button></li>
                             </ul>
                         </Typography>
                     </Grid>
@@ -281,11 +437,11 @@ export class DashboardPage extends React.Component {
                     <Grid item xs={3}>
                         <Typography variant="subheading" gutterBottom align="left">
                             <ul>
-                                <li><a href="#"> 6533 Yewflo Manufacturing </a></li>
-                                <li><a href="#"> 6534 Loop Indicator </a></li>
-                                <li><a href="#"> 6542 Newnan TDLS </a></li>
-                                <li><a href="#"> 6915 Production Control </a></li>
-                                <li><a href="#"> 6931 Manufacturing QA </a></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6533", loadingData: true }) }}> 6533 Yewflo Manufacturing </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6534", loadingData: true }) }}> 6534 Loop Indicator </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6542", loadingData: true }) }}> 6542 Newnan TDLS </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6915", loadingData: true }) }}> 6915 Production Control </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6931", loadingData: true }) }}> 6931 Manufacturing QA </Button></li>
                             </ul>
                         </Typography>
                     </Grid>
@@ -293,11 +449,11 @@ export class DashboardPage extends React.Component {
                     <Grid item xs={2}>
                         <Typography variant="subheading" gutterBottom align="left">
                             <ul>
-                                <li><a href="#"> 6951 Mod Line </a></li>
-                                <li><a href="#"> 6955 Cal Lab </a></li>
-                                <li><a href="#"> 6956 Cal Lab </a></li>
-                                <li><a href="#"> 6957 Cal Lab </a></li>
-                                <li><a href="#"> 8501 R&D </a></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6951", loadingData: true }) }}> 6951 Mod Line </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6955", loadingData: true }) }}> 6955 Cal Lab </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6956", loadingData: true }) }}> 6956 Cal Lab </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC6957", loadingData: true }) }}> 6957 Cal Lab </Button></li>
+                                <li><Button variant="text" onClick={() => { this.setState({ costCenterSelected: "CC8501", loadingData: true }) }}> 8501 R&D </Button></li>
                             </ul>
                         </Typography>
                     </Grid>
