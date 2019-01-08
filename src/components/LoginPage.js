fb19448a6638c812a0a40d88fbf1423c81edace7
Villasana-Espinoza <30035412@us.ykgw.net>
2019-01-08 09:30:25 -0500

    prettier

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,123 +1,137 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { login } from "../redux/auth-reducer"
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import Key from '@material-ui/icons/VpnKey';
-import Button from '@material-ui/core/Button';
-import Snackbar from "@material-ui/core/Snackbar"
-import Redirect from "react-router-dom/Redirect"
+import React from "react";
+import { connect } from "react-redux";
+import { login } from "../redux/auth-reducer";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Key from "@material-ui/icons/VpnKey";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import Redirect from "react-router-dom/Redirect";
 
 export class LoginPage extends React.Component {
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      ID: "",
-      password: ""
-    };
-    this.onSubmit = this.onSubmit.bind(this);
-    console.log("IN THE LOGIN COMP");
-    console.log(props);
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    let { ID, password } = this.state;
-    this.props.login(ID, password);
-    this.setState({
-      ID: '',
-      password: ''
-    });
-  }
-
-
-
-
-  render() {
-    let { ID, password } = this.state;
-    let { isLoginPending, isLoginSuccess, loginError, userPath } = this.props;
-    return (
-      <div className="login-page-class">
-        <Paper className="loginPaper">
-          <div className="loginheaderpart">
-            <Typography variant="display3" gutterBottom className="loginpageheader">
-              Login
-            </Typography>
-          </div>
-          <p></p>
-          <Typography variant="headline" component="h3">
-            Sign into your account
-          </Typography>
-          <form onSubmit={this.onSubmit}>
-            <div className="loginformgroup">
-              <AccountCircle />
-              <TextField
-                id="outlined-email-input"
-                label="YCA ID"
-                // type="email"
-                name="ID"
-                margin="normal"
-                variant="outlined"
-                onChange={e => this.setState({ ID: e.target.value })}
-                value={ID} />
-
-
-
-            </div>
-            <div className="loginformgroup">
-              <Key />
-              <TextField
-                id="password-input"
-                label="Password"
-                margin="normal"
-                variant="outlined"
-                type="password"
-                name="password"
-                onChange={e => this.setState({ password: e.target.value })}
-                value={password} />
-            </div>
-
-            <Button type="submit" value="login" variant="raised" color="primary">
-              Login
-            </Button>
-            {isLoginPending && <Snackbar open={true} autoHideDuration={6000} message={<span>Logging you in...</span>} />}
-            {isLoginSuccess && userPath != null && <Redirect to={userPath} />}
-            {loginError && <Snackbar open={true} autoHideDuration={6000} message={<span>Login failed: invalid credentials</span>} />}
-          </form>
-
-
-
-        </Paper>
-      </div>
-    );
-  }
-
-}
-
-
-
-const mapStateToProps = (state) => {
-  return {
-    isLoginPending: state.isLoginPending,
-    isLoginSuccess: state.isLoginSuccess,
-    loginError: state.loginError,
-    userPath: state.userPath
-  };
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    login: (ID, password) => {
-      login(ID, password)(dispatch);
+    //hello
+    constructor(props) {
+        super(props);
+        this.state = {
+            ID: "",
+            password: ""
+        };
+        this.onSubmit = this.onSubmit.bind(this);
+        console.log("IN THE LOGIN COMP");
+        console.log(props);
     }
-  };
+
+    onSubmit(e) {
+        e.preventDefault();
+        let { ID, password } = this.state;
+        this.props.login(ID, password);
+        this.setState({
+            ID: "",
+            password: ""
+        });
+    }
+
+    render() {
+        let { ID, password } = this.state;
+        let { isLoginPending, isLoginSuccess, loginError, userPath } = this.props;
+        return (
+            <div className="login-page-class">
+                <Paper className="loginPaper">
+                    <div className="loginheaderpart">
+                        <Typography
+                            variant="display3"
+                            gutterBottom
+                            className="loginpageheader"
+                        >
+                            Login
+                        </Typography>
+                    </div>
+                    <p />
+                    <Typography variant="headline" component="h3">
+                        Sign into your account
+                    </Typography>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="loginformgroup">
+                            <AccountCircle />
+                            <TextField
+                                id="outlined-email-input"
+                                label="YCA ID"
+                                // type="email"
+                                name="ID"
+                                margin="normal"
+                                variant="outlined"
+                                onChange={e => this.setState({ ID: e.target.value })}
+                                value={ID}
+                            />
+                        </div>
+                        <div className="loginformgroup">
+                            <Key />
+                            <TextField
+                                id="password-input"
+                                label="Password"
+                                margin="normal"
+                                variant="outlined"
+                                type="password"
+                                name="password"
+                                onChange={e =>
+                                    this.setState({ password: e.target.value })
+                                }
+                                value={password}
+                            />
+                        </div>
+
+                        <Button
+                            type="submit"
+                            value="login"
+                            variant="raised"
+                            color="primary"
+                        >
+                            Login
+                        </Button>
+                        {isLoginPending && (
+                            <Snackbar
+                                open={true}
+                                autoHideDuration={6000}
+                                message={<span>Logging you in...</span>}
+                            />
+                        )}
+                        {isLoginSuccess && userPath != null && (
+                            <Redirect to={userPath} />
+                        )}
+                        {loginError && (
+                            <Snackbar
+                                open={true}
+                                autoHideDuration={6000}
+                                message={<span>Login failed: invalid credentials</span>}
+                            />
+                        )}
+                    </form>
+                </Paper>
+            </div>
+        );
+    }
 }
 
+const mapStateToProps = state => {
+    return {
+        isLoginPending: state.isLoginPending,
+        isLoginSuccess: state.isLoginSuccess,
+        loginError: state.loginError,
+        userPath: state.userPath
+    };
+};
 
+const mapDispatchToProps = dispatch => {
+    return {
+        login: (ID, password) => {
+            login(ID, password)(dispatch);
+        }
+    };
+};
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(LoginPage);
